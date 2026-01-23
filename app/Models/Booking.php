07eb9899074518ca59349646c5a7e20f4af6462d
JuanjoSG5 <113671548+JuanjoSG5@users.noreply.github.com>
2026-01-23 10:03:09 +0100

    fix: added the has factory to booking

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Booking extends Model
 {
+
+    use hasFactory;
     /**
      * The attributes that are mass assignable.
      *
